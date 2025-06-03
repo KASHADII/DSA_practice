bf609215a8e6d1a7b77f54b7aad0acf05f916f5c
Aditya Sharma <132332975+KASHADII@users.noreply.github.com>
2025-06-03 11:45:47 +0530

    Learnt the basics of cpp

---
+++ LearnCpp.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printName(){
+    cout<<"Hey Aditya";
+}
+
+int sum(int num1 , int num2){
+    int num3 = num1 + num2;
+    return num3;
+}
+
+int main(){
+    // // int 
+    // int x = 10; 
+    // // long
+    // long z = 15;
+    // long long y = 150000000000;
+    // // float , double
+    // float x1 = 5.6;
+    // double a = 4.5;
+
+    // string str;
+    // getline(cin , str);
+    // cout << str;
+
+    // char
+    // char ch = 'a';
+
+    // Write a program that takes an input of age and prints if you are an adult or not
+    // int age;
+    // cout<< "Tell me your age: ";
+    // cin>>age;
+
+    // if (age>= 18)
+    // {
+    //     /* code */
+    //     cout<<"You are an adult";
+    // }
+    // else{
+    //     cout<<"You are not an adult";
+    // }
+
+    // int marks;
+    // cin>>marks;
+
+    // if(marks < 25){
+    //     cout<< "F";
+    // }
+    // else if(marks<=44){
+    //     cout<<"E";
+    // }
+    // else if(marks<=49){
+    //     cout<<"D";
+    // }
+    // else if(marks<=59){
+    //     cout<<"C";
+    // }
+    // else if(marks<=79){
+    //     cout<<"B";
+    // }
+    // else if(marks<=100){
+    //     cout<<"A";
+    // }
+
+    // int day;
+    // cin>>day;
+
+    // switch(day){
+    //     case 1:
+    //         cout<<"Monday";
+    //         break;
+    //     case 2:
+    //         cout<<"Tuesday";
+    //         break;
+    //     case 3:
+    //         cout<<"Wednesday";
+    //         break;
+    //     case 4:
+    //         cout<<"Thursday";
+    //         break;
+    //     case 5:
+    //         cout<<"Friday";
+    //         break;
+    //     case 6:
+    //         cout<<"Saturday";
+    //         break;
+    //     case 7:
+    //         cout<<"Sunday";
+    //         break;
+    // }
+
+    // int arr[5];
+    // cin>>arr[0];
+    // cout<<arr[0];
+    // int arr1[5] = {1,2,3,4,5};
+
+    // 2D Array
+    // int arr2D[3][5];
+    // arr2D[1][3] = 78;
+
+    // cout<<arr2D[1][3];
+
+
+    // for loops
+    // for(int i = 1 ; i<=10 ; i++){
+    //     cout<<"Aditya" <<endl;
+    // }
+    // int i;
+    // while(i<=5){
+    //     cout<<"Hello";
+
+    //     i++;
+    // }
+
+    // functions
+    // void -> does not return any value
+    printName();
+    
+    int res = sum(4, 5);
+    cout<<res;
+
+    
+
+
+    
+    return 0;
+}
